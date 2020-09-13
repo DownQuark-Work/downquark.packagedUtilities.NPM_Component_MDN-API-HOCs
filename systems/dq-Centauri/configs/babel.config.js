4308280419c7881ee035f8466497d325e544cc1c
dq-mlnck <mlnck@downquark.work>
2020-09-13 09:32:43 -0400

    Universals incorporated

--- systems/dq-Centauri/configs/babel.config.js
+++ systems/dq-Centauri/configs/babel.config.js
@@ -2,8 +2,4 @@ const baseConfig = require('../../dq_universals/globals/configs/babel.config.js'
 
 module.exports = {
   ...baseConfig,
-  globals: {
-    ...baseConfig.globals,
-    anotherGlobal: true,
-  },
 }
